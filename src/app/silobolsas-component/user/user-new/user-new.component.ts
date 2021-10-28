bf6a9eb0b5c35c9e5393a5e5816c53f11126547e
fantito <crixalis@gmail.com>
2021-10-27 22:44:10 -0300

    lista y new campo

--- src/app/silobolsas-component/user/user-new/user-new.component.ts
+++ src/app/silobolsas-component/user/user-new/user-new.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { User} from '../models/user.model'
+import { User } from '../models/user.model'
 
 @Component({
   selector: 'app-user-new',
